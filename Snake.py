1c05e8990d5e9bea06045d8aacae156d4f931dc7
Ivan L <ileungcw@gmail.com>
2024-11-23 21:38:16 +0800

    MainFourthCommit

--- Snake.py
+++ Snake.py
@@ -78,7 +78,7 @@ def gameLoop():
             
             # Display current number of lives at the top-right corner of the window
             value = score_font.render("Lives: " + str(lives), True, colors['white'])
-            dis.blit(value, [dis_width - 160, 10])
+            dis.blit(value, [dis_width - 170, 10])
             
             pygame.display.update()
 
@@ -144,7 +144,7 @@ def gameLoop():
         
         # Display current number of lives at the top-right corner of the window
         value = score_font.render("Lives: " + str(lives), True, colors['white'])
-        dis.blit(value, [dis_width - 150, 10])
+        dis.blit(value, [dis_width - 170, 10])
 
         pygame.display.update()
 
